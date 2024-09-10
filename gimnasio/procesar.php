2e8c9836c8bd5e3f007e17b511ab37127ab43f4d
Gabriel Arcos <cbi2223046662@izt.uam.mx>
2024-09-10 09:50:50 -0600

    Corrección procesar.php

--- gimnasio/procesar.php
+++ gimnasio/procesar.php
@@ -11,20 +11,19 @@
     $sucursal = $_POST["sucursal"];
     $direccion = $_POST["direccion"];
     $contacto = $_POST["contacto"];
-    $apertura = $_POST["apertura"];
-    $cierre = $_POST["cierre"];
+    $apertura = $_POST["apertura"]. ':00';
+    $cierre = $_POST["cierre"] . ':00';
+	
+	echo "<script>alert('Sucursal: $sucursal . Dirección: $direccion . Contacto: $contacto . Apertura: $apertura . Cierre: $cierre')</script>";
 
     // Insertar los datos en la base de datos
-    $sql = "INSERT INTO sucursales (sucursal, direccion, apertura, cierre, telefono) VALUES ('$sucursal', '$direccion', '$apertura', '$cierre', '$contacto')";
+    $sql = "INSERT INTO sucursal (sucursal, direccion, apertura, cierre, telefono) VALUES ('$sucursal', '$direccion', '$apertura', '$cierre', '$contacto')";
     if ($conexion->query($sql) === TRUE) {
         echo "<script>alert('Registro guardado correctamente');</script>";
     } else {
-        echo "<script>alert('Error al insertar el registro: " . $conexion->error . ");</script>";
+        echo "<script>alert(\Error al insertar el registro: " . $conexion->error . "');</script>";
     }
 
     // Cerrar la conexión con la base de datos
     $conexion->close();
-
-    // Redireccionar a la página de inicio
-    header("Location: index.php");
 ?>
